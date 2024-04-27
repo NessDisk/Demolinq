6544dbd852263f0689fd8ffbffef4dccfae67bff
Marcel Solera <marcel@nubitz.io>
2024-04-26 22:23:12 -0500

    Final correction

--- demo-limb/Program.cs
+++ demo-limb/Program.cs
@@ -5,6 +5,42 @@
 LinqQueries queries = new LinqQueries();
 
 // ----
+// Funciones para imprimir las queris en la terminal. 
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+void ImprimirGrupo(IEnumerable<IGrouping<int, Book>> ListadeLibros)
+{
+    foreach (var grupo in ListadeLibros)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Grupo: {grupo.Key}");
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+        foreach (var item in grupo)
+        {
+            Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+        }
+    }
+}
+
+
+void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var item in ListadeLibros[letra])
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+    }
+}
+
+// ----
 
 // ImprimirValores(queries.TodaLaColeccion());
 
@@ -16,38 +52,6 @@ LinqQueries queries = new LinqQueries();
 
 
 
-void ImprimirValores(IEnumerable<Book> listadelibros)
-{
-    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
-    foreach(var item in listadelibros)
-    {
-        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
-    }
-}
-
-void ImprimirGrupo(IEnumerable<IGrouping<int,Book>> ListadeLibros)
-{
-    foreach(var grupo in ListadeLibros)
-    {
-        Console.WriteLine("");
-        Console.WriteLine($"Grupo: { grupo.Key }");
-        Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
-        foreach(var item in grupo)
-        {
-            Console.WriteLine("{0,-60} {1, 15} {2, 15}",item.Title,item.PageCount,item.PublishedDate.Date.ToShortDateString()); 
-        }
-    }
-}
-
-
-void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
-{
-   Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
-   foreach(var item in ListadeLibros[letra])
-   {
-         Console.WriteLine("{0,-60} {1, 15} {2, 15}",item.Title,item.PageCount,item.PublishedDate.Date.ToShortDateString()); 
-   }
-}
 
 // ----
 //Ejemplo de operador All y Any
@@ -65,49 +69,49 @@ void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
 // ImprimirValores(queries.LibrosConMasDe450PagDesn()); 
 
 // Ejemplo del operador Take. Se trae los primeros cuatro libros de la condicion.
-    // ImprimirValores(queries.Primeros4LibrosDeJava());
+// ImprimirValores(queries.Primeros4LibrosDeJava());
 
 // Ejemplo del operador Skip, Que salta una cierta cantidad de valores de la lista.
-    // ImprimirValores(queries.TercerYCuartoLibroConMasDe400Pags());
+// ImprimirValores(queries.TercerYCuartoLibroConMasDe400Pags());
 
 // Ejemplo de Select
 // Ya que solo hay hay 2 tables la fecha es nula y la funcion la pone por defecto como "1/01/0001"
 // Para conseguir eliminar la table y evitar errores lo mas conveniente es hacer un Tipo nuevo
 // Solo con esas dos tablas.
-    // ImprimirValores(queries.SelectTablaNombreYNumeroDePaginas());
+// ImprimirValores(queries.SelectTablaNombreYNumeroDePaginas());
 
-    // Ejemplo del operador Count cuenta el tamaño de la lista.
-    // Console.WriteLine( "La cantidad de libros "+ queries.CountLibrosEntre200Y500Paginas());
+// Ejemplo del operador Count cuenta el tamaño de la lista.
+// Console.WriteLine( "La cantidad de libros "+ queries.CountLibrosEntre200Y500Paginas());
 
 // Ejemplo del operador Min
-    // Console.WriteLine( "Libro con la menor fecha de publicación "+ queries.LibroConMenorFechaDePublicacion());
+// Console.WriteLine( "Libro con la menor fecha de publicación "+ queries.LibroConMenorFechaDePublicacion());
 
 
 // Ejemplo del operador Max
-    // Console.WriteLine( "Libro con el mayor numero de paginas "+ queries.LibroMayorNumeroDePaginas());
+// Console.WriteLine( "Libro con el mayor numero de paginas "+ queries.LibroMayorNumeroDePaginas());
 
 // Ejemplo operador MinBy
-    // var libroMenorNumeroDepaginas = queries.LibroConLaMenorCantidadDePaginasMayorQueZero();
-    // Console.WriteLine(  $"{libroMenorNumeroDepaginas.Title}  -  {libroMenorNumeroDepaginas.PageCount}");
+// var libroMenorNumeroDepaginas = queries.LibroConLaMenorCantidadDePaginasMayorQueZero();
+// Console.WriteLine(  $"{libroMenorNumeroDepaginas.Title}  -  {libroMenorNumeroDepaginas.PageCount}");
 
 // Ejemplo operador MaxBy
-    // var libroMayorFechaPublicacion = queries.LibroConLaFechaDePublicacionMasReciente();
-    // Console.WriteLine(  $"{libroMayorFechaPublicacion.Title}  -  {libroMayorFechaPublicacion.PublishedDate}");
+// var libroMayorFechaPublicacion = queries.LibroConLaFechaDePublicacionMasReciente();
+// Console.WriteLine(  $"{libroMayorFechaPublicacion.Title}  -  {libroMayorFechaPublicacion.PublishedDate}");
 
 // Ejemplo de operador Sum
-    // Console.WriteLine( "La sumatoria de todos los libros de 200 a 500 paginas es: " + queries.SumaDeTodaslasPaginasMayores500());
+// Console.WriteLine( "La sumatoria de todos los libros de 200 a 500 paginas es: " + queries.SumaDeTodaslasPaginasMayores500());
 
 // Ejemplo de operador Aggregate. que concatena los valores en un string.
-    // Console.WriteLine(queries.TitulosLibrosConcatenado());
+// Console.WriteLine(queries.TitulosLibrosConcatenado());
 
 // Ejemplo de operador Average. Da el promedio de una coleccion de numeros. 
 // Console.WriteLine(" Promedio "+queries.PromedioCaracteresDeLaColeccion());
 
 // Ejemplo de operador Average. Da el promedio de una coleccion de numeros. 
-    // Console.WriteLine(" Promedio paginas libro "+queries.PromedioPaginaLibro());
+// Console.WriteLine(" Promedio paginas libro "+queries.PromedioPaginaLibro());
 
 // Ejemplo de Clausula GroupBy(). agrupo dependiendo una clausula. 
-    // ImprimirGrupo(queries.AgruparLibrosPosterioresALos2000());
+// ImprimirGrupo(queries.AgruparLibrosPosterioresALos2000());
 
 // Ejemplo de Clausula LookUp, que agrupo como si fuera un diccionario
 // siendo que si uno le solucita una letra se trae todo ese grupo
